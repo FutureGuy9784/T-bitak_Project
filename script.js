3f2f5c3cc3c05a6b556bec970bfe149d2e6d2afa
Ali Ağaoğlu <142357735+FutureGuy9784@users.noreply.github.com>
2024-01-06 11:29:32 +0300

    5ııu73

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var otomatikParagraf = document.getElementById("otomatikParagraf");
+    otomatikParagraf.style.display = "block";
+});
